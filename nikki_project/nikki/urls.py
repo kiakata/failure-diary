787c2b1eb20c55da30b3b78588c3597757c11f64
Takaaki Uno <takaaki.uno@gmail.com>
2018-05-29 13:53:24 +0900

    [add] refs #4 編集するボタン、削除確認ページを追加

--- nikki_project/nikki/urls.py
+++ nikki_project/nikki/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('index', views.ArticleList.as_view(), name='index'),
     # path('create_article/<int:user_id>', views.CreateArticle.as_view(), name='create_article'),
     path('create_article/<int:user_id>', views.create_article, name='create_article'),
-    path('detail_article/<int:pk>', views.DetailArticle.as_view(), name='detail_article')
+    path('detail_article/<int:pk>', views.DetailArticle.as_view(), name='detail_article'),
+    path('update_article/<int:pk>', views.UpdateArticle.as_view(), name='update_article'),
+    path('delete_article/<int:pk>', views.DeleteArticle.as_view(), name='delete_article')
 ]
