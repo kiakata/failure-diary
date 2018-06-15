443548b9f1190940a9e51c3e4f6ad85c818ee4b9
Takaaki Uno <takaaki.uno@gmail.com>
2018-06-16 01:56:07 +0900

    [fix] fixed heroku.py

--- nikki_project/nikki_project/settings/heroku.py
+++ nikki_project/nikki_project/settings/heroku.py
@@ -3,8 +3,11 @@ from .common import *
 import dj_database_url
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+if os.environ.get('debug', False):
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
