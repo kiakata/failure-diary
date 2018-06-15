0795d13d3a34d87f3f6d7d020d80d61e9c8592a6
Takaaki Uno <takaaki.uno@gmail.com>
2018-06-16 01:16:54 +0900

    [fix] undo changes in mange.py and wsgi.py

--- nikki_project/nikki_project/wsgi.py
+++ nikki_project/nikki_project/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nikki_project.nikki_project.settings.heroku")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nikki_project.settings.heroku")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
